adb67ac4c28ac0327d5bae163debce21d20dad91
cevenaris <cyzsidelt@gmail.com>
2021-09-21 16:57:46 -0700

    created the libgdx project

---
+++ android/src/io/github/cevenaris/qmi/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package io.github.cevenaris.qmi;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.github.cevenaris.qmi.QuickMusicInfo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new QuickMusicInfo(), config);
+	}
+}
